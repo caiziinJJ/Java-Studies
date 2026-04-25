a413b639c773160f80013867604487a0fac7be1b
Caio Danilo <caiodanilo@live.com>
2026-04-25 13:08:03 -0300

    Detecta parcialmente se a coluna é básica ou não

--- simplex/Simplex.java
+++ simplex/Simplex.java
@@ -13,7 +13,7 @@ public class Simplex{
         this.matriz = new double[linhas][colunas];
     }
 
-    public void criarMatriz(){
+    public void preencherMatriz(){
         for(int i = 0; i < this.linhas; i++) {
             for (int j = 0; j < this.colunas; j++) {
                 System.out.printf("Você está na linha %d, coluna %d. Digite o valor: %n", i+1, j+1);
@@ -33,16 +33,72 @@ public class Simplex{
     }
 
     public void simplexBasico(double [][] matriz){
-        for(int i = 0; i < this.colunas; i++){ //Aqui vou achar o menor número de "b"
-              
+        //Primeiro eu vou printar a matriz antes das iterações
+        int iteracoes = 0;
+        System.out.printf("Quantidade de iterações: %d%n", iteracoes);
+        //Aqui eu vou identificar as minhas colunas básicas
+        int [] posicoesVariaveisBasicas = new int[0];
+        int contadorColunasBasicas = 0;
+        for (int j = 0; j< matriz[0].length; j++){
+            boolean colunaBasica = true;
+            int posicaoUm = -1;
+            for (int i = 0; i < matriz.length; i++){
+                if(matriz[i][j] == 0 || matriz[i][j] == 1){
+                    if (matriz [i][j] == 1){
+                        posicaoUm = i;
+                    }
+                }
+                else{
+                    colunaBasica = false;
+                    break;
+                }
+            }
+            if(colunaBasica && posicaoUm != 0){
+                contadorColunasBasicas++;
+            }
         }
+        System.out.printf("Quantidade de colunas básicas: %d%n",contadorColunasBasicas);
+        //aqui eu crio a variável que armazena o menor número da linha "b"
+        double menorNumeroArray = 0;
+        //aqui eu armazeno a coluna onde encontramos o número mais negativo
+        int posicaoColunaInferior = 0;
+        //aqui eu crio um array que vai conter os elementos da linha "b". Eles são usados
+        //para definirmos qual variável se tornará básica através do cálculo que eu esqueci
+        //o nome. Vou negativá-los para eles voltarem ao "original"
+        double [] elementosFuncaoMae = new double[matriz[0].length];
+        for(int j = 0; j < matriz[0].length; j++){
+           elementosFuncaoMae[j] = matriz[0][j];
+           for(int i = 0; i < elementosFuncaoMae.length ; i++){
+               elementosFuncaoMae[i] = elementosFuncaoMae[i] * - 1;
+           }
+        }
+        for (int i = 0; i < elementosFuncaoMae.length; i++){
+            System.out.printf("%8.2f", elementosFuncaoMae[i]);
+        }
+        /*
+        for(int j = 0; j < this.matriz[0].length; j++){
+            if(menorNumeroArray > matriz[matriz.length - 1][j]){
+                menorNumeroArray = matriz[matriz.length - 1][j];
+                posicaoColunaInferior = j;
+            }
+        }
+
+         */
     }
 
     public static void main(String[] args){
-        System.out.println("Teste");
-        Simplex t1 = new Simplex("m1", 4, 4);
-        t1.criarMatriz();
-        t1.verMatriz();
+        double [][] matrizSimplex = {
+                {1,1,1,1,0,0,0,40},
+                {2,1,-1,0,1,0,0,20},
+                {3,2,-1,0,0,1,0,30},
+                {-2,-3,-1,0,0,0,1,0}
+        };
+        Simplex t1 = new Simplex("m1", 4, 8);
+        //System.out.println(t1.matriz[0].length);
+        System.out.println();
+        t1.simplexBasico(matrizSimplex);
+        //t1.preencherMatriz();
+        //.verMatriz();
         
         
     }
